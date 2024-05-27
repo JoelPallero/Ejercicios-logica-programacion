67f0aa475938eb08064636a695b9d3d1c2925bd7
Joel Pallero <estebanjoelpallero@hotmail.com>
2024-05-27 13:57:34 -0300

    Es primo en PHP

---
+++ 04-NUMERO-PRIMO/php/isPrime.php
@@ -0,0 +1,46 @@
+<?php
+
+function checkNumbers($n){
+    if($n === 0) {
+        return false;
+    }
+    else if($n === 1) {
+        return false;
+    }
+    else if($n === 2) {
+        return true;
+    }
+
+    for ($i=2; $i <= sqrt($n); $i++) { 
+        if($n % $i === 0){
+            return false;
+        }
+    }    
+    return true;
+}
+
+function displayPrimeNumbers(){
+    for ($i=0; $i < 100; $i++) {
+        if(checkNumbers($i)){
+            echo '<span>'.$i.'</span><br>';
+        }
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fibonacci</title>
+</head>
+<body>
+
+<div class="app">
+    <?php displayPrimeNumbers(); ?>
+</div>
+    
+</body>
+</html>
