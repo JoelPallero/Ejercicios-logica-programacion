cbe4c0245159f1b9c48340ad01c32e77f78792e1
Joel Pallero <estebanjoelpallero@hotmail.com>
2024-05-27 14:15:33 -0300

    Es primo en react.js

--- 04-NUMERO-PRIMO/php/isPrime.php
+++ 04-NUMERO-PRIMO/php/isPrime.php
@@ -34,7 +34,7 @@ function displayPrimeNumbers(){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fibonacci</title>
+    <title>Es Primo</title>
 </head>
 <body>
 
--- 04-NUMERO-PRIMO/react.js/src/App.jsx
+++ 04-NUMERO-PRIMO/react.js/src/App.jsx
@@ -1,32 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [primeNumbers, setPrimeNumbers] = useState([]);
+
+  function chekumber(n){
+    if (n === 0) return false;
+    if (n === 1) return false;
+    if (n === 2) return true;
+
+    for(let i = 2; i < Math.sqrt(n); i++){
+      if (n % i === 0) return false;
+    }
+    return true;
+  }
+
+  function chkPrime(){
+    let arrayExtra = [];
+    for(let i = 1; i <= 100; i++){
+      if (chekumber(i)){
+        arrayExtra.push(i);
+      }
+    }
+    setPrimeNumbers(arrayExtra);
+  }
+
+  useEffect(()=>{
+    chkPrime();
+  },[])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p>
+        {primeNumbers.map((prime, index)=>{
+          return <span key={index}>{prime}<br/></span>
+        })}
       </p>
     </>
   )
